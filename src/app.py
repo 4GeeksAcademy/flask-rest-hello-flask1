54d5a1199c219400b7ed38f0d2b2eeb6c0d541ce
lucki200 <90269862+lucki200@users.noreply.github.com>
2024-08-12 16:45:36 +0000

    todo lask

--- src/app.py
+++ src/app.py
@@ -1,51 +1,27 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
-import os
-from flask import Flask, request, jsonify, url_for
-from flask_migrate import Migrate
-from flask_swagger import swagger
-from flask_cors import CORS
-from utils import APIException, generate_sitemap
-from admin import setup_admin
-from models import db, User
-#from models import Person
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    todos = Todo.query.all()
+    todos = list(map(lambda todo: todo.serialize(), todos))
+    return jsonify(todos), 200
 
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    data = request.get_json()
+    new_todo = Todo(label=data['label'], done=data['done'])
+    db.session.add(new_todo)
+    db.session.commit()
+    todos = Todo.query.all()
+    todos = list(map(lambda todo: todo.serialize(), todos))
+    return jsonify(todos), 201
 
-db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todo = Todo.query.get(position)
+    if todo is None:
+        return jsonify({"error": "Todo not found"}), 404
 
-MIGRATE = Migrate(app, db)
-db.init_app(app)
-CORS(app)
-setup_admin(app)
-
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
-
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
-
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
-    return jsonify(response_body), 200
-
-# this only runs if `$ python src/app.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    db.session.delete(todo)
+    db.session.commit()
+    todos = Todo.query.all()
+    todos = list(map(lambda todo: todo.serialize(), todos))
+    return jsonify(todos), 200
